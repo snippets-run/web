dfcd7937a834fae81b7169e5cf71f362bd7330f0
Darlan Alves <me@darlanalv.es>
2024-02-08 17:21:52 +0000

    chore: update layout

--- layout/Footer.vue
+++ layout/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex-grow grid sm:grid-cols-3 gap-8">
+  <div class="flex-grow flex justify-between gap-4 container mx-auto px-8 sm:w-1/2">
     <div class="flex flex-col space-y-1">
       <h1 class="font-semibold">Resources</h1>
       <a
@@ -21,9 +21,8 @@
         >
           <path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path>
           <polyline points="15 3 21 3 21 9"></polyline>
-          <line x1="10" x2="21" y1="14" y2="3"></line>
-        </svg>
-      </a>
+          <line x1="10" x2="21" y1="14" y2="3"></line></svg
+      ></a>
     </div>
     <div class="flex flex-col space-y-1">
       <h1 class="font-semibold">Product</h1>
@@ -43,60 +42,14 @@
         class="text-sm underline flex items-center text-muted-foreground hover:no-underline hover:text-primary"
         href="/legal/terms"
         >Terms</a
-      >
-      <a
+      ><a
         class="text-sm underline flex items-center text-muted-foreground hover:no-underline hover:text-primary"
         href="/legal/privacy"
         >Privacy</a
       >
     </div>
   </div>
-  <div class="mt-8 border-t w-full pt-4 flex items-center justify-between">
-    <div class="text-muted-foreground text-sm">© 2024 snippets.run</div>
-    <button
-      class="inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 border border-input bg-transparent shadow-sm hover:bg-accent hover:text-accent-foreground h-9 w-9"
-      type="button"
-      aria-haspopup="menu"
-      aria-expanded="false"
-      data-state="closed"
-    >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        class="lucide lucide-sun h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
-      >
-        <circle cx="12" cy="12" r="4"></circle>
-        <path d="M12 2v2"></path>
-        <path d="M12 20v2"></path>
-        <path d="m4.93 4.93 1.41 1.41"></path>
-        <path d="m17.66 17.66 1.41 1.41"></path>
-        <path d="M2 12h2"></path>
-        <path d="M20 12h2"></path>
-        <path d="m6.34 17.66-1.41 1.41"></path>
-        <path d="m19.07 4.93-1.41 1.41"></path>
-      </svg>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        width="24"
-        height="24"
-        viewBox="0 0 24 24"
-        fill="none"
-        stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
-        class="lucide lucide-moon absolute h-6 w-6 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
-      >
-        <path d="M12 3a6 6 0 0 0 9 9 9 9 0 1 1-9-9Z"></path>
-      </svg>
-      <span class="sr-only">Toggle theme</span>
-    </button>
+  <div class="mt-8 border-t py-4 px-8 flex items-center justify-between container mx-auto">
+    <div class="text-muted-foreground text-sm">© snippets.run</div>
   </div>
 </template>
--- layout/Header.vue
+++ layout/Header.vue
@@ -1,62 +1,43 @@
 <template>
-  <div class="p-4 border-b flex items-center justify-between sticky top-0 bg-background z-50">
-    <div class="flex items-center space-x-4">
-      <a class="relative w-8 h-8 flex-shrink-0" href="/home"
-        ><img
-          alt="/logo"
-          loading="lazy"
-          decoding="async"
-          data-nimg="fill"
-          style="
-            position: absolute;
-            height: 100%;
-            width: 100%;
-            left: 0;
-            top: 0;
-            right: 0;
-            bottom: 0;
-            color: transparent;
-          "
-          sizes="100vw"
-      /></a>
-      <div>
-        <input
-          class="h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 hidden sm:block"
-          placeholder="Search..."
-          value=""
-        />
+  <div class="p-4 border-b sticky top-0 bg-white z-50">
+    <div class="flex items-center justify-items-stretch space-x-4 w-full">
+      <a class="w-16 h-11 block" href="/home"><img alt="/logo" src="/assets/icon-a0698e3a.svg" /></a>
+      <div class="w-full">
+        <h1 class="font-semibold">snippets.run</h1>
+        <p class="text-sm">A repository for runnable code snippets</p>
       </div>
-      <a class="font-semibold hidden sm:block" href="/me">My Snippets</a>
-      <a class="font-semibold hidden sm:block" href="/discover">All Snippets</a>
-    </div>
-    <div class="flex items-center space-x-4">
-      <a class="font-semibold" href="/new"
-        ><svg
-          xmlns="http://www.w3.org/2000/svg"
-          width="24"
-          height="24"
-          viewBox="0 0 24 24"
-          fill="none"
-          stroke="currentColor"
-          stroke-width="2"
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          class="lucide lucide-plus w-5 h-5"
+      <input
+        class="h-9 w-72 rounded-md border border-input bg-transparent px-3 py-1 text-sm shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 hidden sm:block"
+        placeholder="Search..."
+        value=""
+      />
+      <div class="flex items-center space-x-4">
+        <a class="font-semibold" href="/new"
+          ><svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="lucide lucide-plus w-5 h-5"
+          >
+            <path d="M5 12h14"></path>
+            <path d="M12 5v14"></path></svg></a
+        ><button
+          class="inline-block rounded-full w-8 h-8 overflow-hidden"
+          type="button"
+          id="radix-:R55nlla:"
+          aria-haspopup="menu"
+          aria-expanded="false"
+          data-state="closed"
         >
-          <path d="M5 12h14"></path>
-          <path d="M12 5v14"></path></svg></a
-      ><button
-        class="inline-flex items-center justify-center whitespace-nowrap text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground px-4 py-2 relative w-8 h-8 rounded-full"
-        type="button"
-        id="radix-:R55nlla:"
-        aria-haspopup="menu"
-        aria-expanded="false"
-        data-state="closed"
-      >
-        <span class="relative flex shrink-0 overflow-hidden rounded-full w-8 h-8"
-          ><img class="aspect-square h-full w-full" src="https://avatar.vercel.sh/me"
-        /></span>
-      </button>
+          <span><img class="aspect-square h-full w-full" src="https://avatar.vercel.sh/me" /></span>
+        </button>
+      </div>
     </div>
   </div>
 </template>
